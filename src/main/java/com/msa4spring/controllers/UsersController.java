215e4e7cf8704dd5ce564752d93c85f32a6016c4
coding_babo <kwondevelop@gmail.com>
2026-05-13 17:26:01 +0900

    260513 spring boot

--- src/main/java/com/msa4spring/controllers/UsersController.java
+++ src/main/java/com/msa4spring/controllers/UsersController.java
@@ -1,4 +1,21 @@
 package com.msa4spring.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UsersController {
+    
+    @GetMapping("/users")
+    public String users() {
+        return "GET users";
+    }
+    
+    @PostMapping("/users")
+    public String usersPost() {
+        return "POST users";
+    }
 }
